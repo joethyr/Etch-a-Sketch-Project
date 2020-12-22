a2152f0460a623b6d9e2beb8a566238d57ba5400
joethyr <joethayr@gmail.com>
2020-12-21 19:47:33 -0700

    revised the square hover function by creating classes to change the background color. This will make creating a button to reset grid colors much easier.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+function changeColor() {
+  $(this).css("background", "white");
+  console.log("hello");
+}
+
 $(document).ready(function () {
-  $(".square").hover(
-    function () {
-      $(this).css("background", "black");
-    },
-    function () {
-      $(this).css("background", "black");
-    }
-  );
+  $(".square").hover(function () {
+    $(this).addClass("toggle");
+  });
 });
 
 for (i = 0; i < 256; i++) {
   const grid = document.createElement("div");
   grid.className = "square";
+
   document.getElementById("grid").appendChild(grid);
 }
