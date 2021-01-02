6b8d2c033896f97e22bf8c006db8bbc1950e8dad
joethyr <joethayr@gmail.com>
2021-01-02 16:32:02 -0700

    added function that cretes grid based on the provided squares per side. This will enable users to adjust the grid size.

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+const gridContainer = document.querySelector("#grid-container");
+let squaresPerSide = "";
+
 $(document).ready(function () {
   $(".square").hover(function () {
     $(this).addClass("toggle");
   });
 });
 
-for (i = 0; i < 256; i++) {
-  const grid = document.createElement("div");
-  grid.className = "square";
-
-  document.getElementById("grid").appendChild(grid);
-}
-
 $(document).ready(function () {
-  $("#clearButton").click(function () {
+  $("#clear-button").click(function () {
     $(".square").removeClass("toggle");
   });
 });
+
+function createGrid(squaresPerSide) {
+  gridContainer.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr`;
+  gridContainer.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr`;
+  let squaresTotal = squaresPerSide * squaresPerSide;
+  for (i = 0; i < squaresTotal; i++) {
+    const grid = document.createElement("div");
+    grid.className = "square";
+    document.getElementById("grid-container").appendChild(grid);
+  }
+}
+
+createGrid(16);
