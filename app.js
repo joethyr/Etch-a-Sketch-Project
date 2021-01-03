d3d8aaafe0579af5f00de0817aac6dd0d7c437e0
joethyr <joethayr@gmail.com>
2021-01-03 15:38:57 -0700

    cleaned up code and added comments to explain functions.

--- app.js
+++ app.js
@@ -1,33 +1,47 @@
 const gridContainer = document.querySelector("#grid-container");
 let squaresPerSide = "";
 
-$(function () {
+// define the function that toggles the squares on the grid
+function toggleSquares() {
   $(".square").hover(function () {
     $(this).addClass("toggle");
   });
+}
+
+// call the function
+$(document).ready(function () {
+  toggleSquares();
 });
 
+//prompts the user when the button is clicked and runs the createGrid function.
 $(function () {
   $("#grid-button").click(function () {
     squaresPerSide = prompt(
       "How many squares do you want on each side of the grid?"
     );
-    removeCells();
     createGrid(squaresPerSide);
   });
 });
 
+//function that removes the toggle class from the grid squares
 $(document).ready(function () {
   $("#clear-button").click(function () {
     $(".square").removeClass("toggle");
   });
 });
 
+// removes the square class from the gridContainer parent
+function removeSquares() {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
+// function that creates a grid based on the value provided on the 'grid-button' prompt
 function createGrid(squaresPerSide) {
-  $(function () {
-    $(".square").hover(function () {
-      $(this).addClass("toggle");
-    });
+  removeSquares();
+  $(document).ready(function () {
+    toggleSquares();
   });
   gridContainer.style.gridTemplateColumns = `repeat(${squaresPerSide}, 1fr`;
   gridContainer.style.gridTemplateRows = `repeat(${squaresPerSide}, 1fr`;
@@ -39,10 +53,5 @@ function createGrid(squaresPerSide) {
   }
 }
 
-function removeCells() {
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild);
-  }
-}
-
+// execute function with requested value
 createGrid(16);
